7d009f9c0aa46178c59157f7616c894587b9edee
Moksedul <moksedulali2006@gmail.com>
2026-06-06 13:27:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function showSection(sectionId){
 window.onload = function(){
   showSection("home");
 };
+function updateClock(){
+
+  let now = new Date();
+
+  let time = now.toLocaleTimeString();
+
+  let date = now.toDateString();
+
+  document.getElementById("clock").textContent = time;
+  document.getElementById("date").textContent = date;
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
