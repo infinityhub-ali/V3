db450851993f2b5b121af0ec2beb0ae61cbde6c4
Moksedul <moksedulali2006@gmail.com>
2026-06-06 13:13:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function showSection(sectionId){
 
+  let sections = document.querySelectorAll("main section");
+
+  sections.forEach(function(section){
+    section.style.display = "none";
+  });
+
+  document.getElementById(sectionId).style.display = "block";
+}
+
+window.onload = function(){
+  showSection("home");
+};
